fa36b9892ef225d58ac6e608febe388a0d355de9
sahanRoshitha99 <107123538+sahanRoshitha99@users.noreply.github.com>
2026-09-19 15:55:52 +0530

    Initial project setup with MySQL and folder structure

---
+++ src/main/java/com/mapleandcrumb/util/DBConnection.java
@@ -0,0 +1,15 @@
+package com.mapleandcrumb.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/maple_crumb_db";
+    private static final String USER = "root"; // Change to your MySQL username
+    private static final String PASSWORD = "root"; // Change to your MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
